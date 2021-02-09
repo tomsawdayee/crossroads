0f13ddecdf81a54719c754643d833e81ed580dc8
Asaf Mor <asaf.mor@sap.com>
2021-02-10 00:03:28 +0200

    Fix decision processing

--- src/Play.vue
+++ src/Play.vue
@@ -51,13 +51,13 @@
     </div>
 
     <div id="action-container" v-if="!this.gameEnded">
-      <button id="postpone-button" v-on:click="reject" class="action-button" v-if="renderedItem.canPostpone">
+      <button id="postpone-button" v-on:click="decide('postpone')" class="action-button" v-if="renderedItem.canPostpone">
         <font-awesome-icon size="3x" :icon="['fas', 'hourglass-half']" :style="{ color: 'white' }" />
       </button>
-      <button id="reject-button" v-on:click="reject" class="action-button">
+      <button id="reject-button" v-on:click="decide('no')" class="action-button">
         <font-awesome-icon size="3x" :icon="['fas', 'times']" :style="{ color: 'white' }"/>
       </button>
-      <button id="accept-button" v-on:click="approve" class="action-button">
+      <button id="accept-button" v-on:click="decide('yes')" class="action-button">
         <font-awesome-icon size="3x" :icon="['fas', 'check']" :style="{ color: 'white' }"/>
       </button>
     </div>
@@ -69,63 +69,68 @@
 <script>
 import Profile from "./views/Profile.vue";
 
+//check enough energy/enough money
+//check an option of delay between decisions
+//add effect in reject/approve
+//greater weights
+//every effect should have minus value too
+//shorter page
+//game over two option image
+//youre too tired in order to continue
+//the decisions for today are over
+//if (enough energy) end game DONE
+//add swipe
+
 export default {
   name: 'Play',
   components: {Profile},
   methods: {
-    approve: function (event) {
-      let index = this.currentIndex
-      const decision = this.decisions[index]
-
-      //check enough energy/enough money
-      //check an option of delay between decisions
-      //add effect in reject/approve
-      //greater weights
-      //every effect should have minus value too
-      //shorter page
-      //game over two option image
-      //youre too tired in order to continue
-      //the decisions for today are over
-      //if (enough energy) end game DONE
-      //add swipe
-
-      if(decision) {
-        this.user.energy.delta += decision.cost.energy || 0
-        this.user.coins.delta += decision.cost.coins || 0
-
-        this.user.health.delta += decision.effect.health || 0
-        this.user.happiness.delta += decision.effect.happiness || 0
-        this.user.intelligence.delta += decision.effect.intelligence || 0
-        this.user.leadership.delta += decision.effect.leadership || 0
-        this.user.relationships.delta += decision.effect.relationships || 0
-
-        this.currentIndex++
+    decide: function (answer) {
+      if (answer !== 'postpone') {
+        this.processDecision(answer);
       }
 
-      if(this.currentIndex === this.decisions.length) {
-        this.gameEnded = true
-        this.reasonForEnd = this.reasonForEndOptions.DECISIONS
+      this.currentIndex++
+
+      if (this.currentIndex === this.decisions.length) {
+        this.gameEnded = true;
+        this.reasonForEnd = this.reasonForEndOptions.DECISIONS;
       }
 
-      if(this.user.energy === 0) {
-        this.gameEnded = true
-        this.reasonForEnd = this.reasonForEndOptions.ENERGY
+      if (this.user.energy === 0) {
+        this.gameEnded = true;
+        this.reasonForEnd = this.reasonForEndOptions.ENERGY;
       }
 
-      if(this.user.coins === 0) {
-        this.gameEnded = true
-        this.reasonForEnd = this.reasonForEndOptions.COINS
+      if (this.user.coins === 0) {
+        this.gameEnded = true;
+        this.reasonForEnd = this.reasonForEndOptions.COINS;
       }
 
-      if(this.gameEnded){
-        window.qualities = this.user
+      if (this.gameEnded) {
+        this.user.coins.delta -= this.user.revenues;
+        window.qualities = this.user;
       }
     },
-    reject: function (event) {
-      this.currentIndex++
-      this.gameEnded = this.currentIndex === this.decisions.length
-      if(this.gameEnded){
-        window.qualities = this.user
+    processDecision(answer) {
+      const decision = this.decisions[this.currentIndex];
+      if (decision) {
+        let effect = {};
+        if (answer === 'yes') {
+          effect = decision.effect.approved;
+          const cost = decision.cost;
+          this.user.energy.delta += cost.energy || 0;
+          this.user.coins.delta += cost.coins || 0;
+        } else if (answer === 'no') {
+          effect = decision.effect.rejected;
+        }
+
+        this.user.health.delta += effect.health || 0;
+        this.user.happiness.delta += effect.happiness || 0;
+        this.user.intelligence.delta += effect.intelligence || 0;
+        this.user.leadership.delta += effect.leadership || 0;
+        this.user.relationships.delta += effect.relationships || 0;
+        this.user.revenues += effect.coins || 0;
       }
     },
     gameOver: function (event) {
@@ -152,7 +157,7 @@ export default {
   data () {
     return {
       showSplash: true,
-      user: {energy:  {value: 100, delta:0} , coins: {value: 20, delta:0}, health: {value: 20, delta:0}, relationships: {value: 0, delta:0}, intelligence: {value: 0, delta:0}, leadership: {value: 0, delta:0}, happiness: {value: 20, delta:0}},
+      user: {energy:  {value: 100, delta:0} , coins: {value: 20, delta:0}, health: {value: 20, delta:0}, relationships: {value: 0, delta:0}, intelligence: {value: 0, delta:0}, leadership: {value: 0, delta:0}, happiness: {value: 20, delta:0}, revenues: 0},
       currentIndex: 0,
       gameEnded: false,
       showProfile:false,
@@ -165,53 +170,74 @@ export default {
       decisions: [
         {
           id: '1',
-          description: 'Asaf needs help with his homework in math. Would you like to assist?',
+          description: 'Sara asked from you to help with homework in math. Would you like to assist?',
           imageUrl: './assets/Parenting-Help.jpg',
           cost: {energy: 7},
-          effect: {relationships: 2}
+          effect: {
+            approved: { relationships: 3, health: -1 },
+            rejected: { relationships: -3 }
+          }
         },
         {
           id: '2',
           description: 'Would you like to open a Lemonade stand?',
           canPostpone: true,
           imageUrl: './assets/lemonadeStand.jpg',
-          cost: {coins: 3, energy: 10},
-          effect: {relationships: 2, happiness: 2}
+          cost: {coins: 10, energy: 20},
+          effect: {
+            approved: { leadership: 2, coins: 20 },
+            rejected: {}
+          }
         },
         {
           id: '3',
-          description: 'Your Grandmother is sick, would you like to visit her?',
+          description: 'Your Grandmother is sick. Would you like to visit her?',
           imageUrl: './assets/Grandmother.png',
           cost: {energy: 5},
-          effect: {happiness: 2, relationships: 2}
+          effect: {
+            approved: { relationships: 2, happiness: 2 },
+            rejected: {}
+          }
         },
         {
           id: '4',
-          description: 'Would you like to gather a Team and clean a beach?',
+          description: 'Would you like to gather a team of friends and clean the city beach?',
           imageUrl: './assets/cleanBeach.jpg',
           cost: {energy: 10},
-          effect: {leadership: 3, happiness: 1}
+          effect: {
+            approved: { leadership: 3, happiness: 1 },
+            rejected: {}
+          }
         },
         {
           id: '5',
-          description: 'Would you like to go jogging',
+          description: 'Would you like to go out for a short jogging?',
           imageUrl: './assets/GoJogging.jpg',
           cost: {energy: 10},
-          effect: {health: 3}
+          effect: {
+            approved: { health: 3 },
+            rejected: {}
+          }
         },
         {
           id: '6',
-          description: 'Would you like to do meditation',
+          description: 'Would you like to do meditation?',
           imageUrl: './assets/doMeditation.jpg',
           cost: {energy: 2},
-          effect: {health: 3, happiness: 3}
+          effect: {
+            approved: { health: 3, happiness: 3 },
+            rejected: {}
+          }
         },
         {
           id: '7',
           description: 'How about going to the university?',
           imageUrl: './assets/GoToUniversity.jpg',
           cost: {coins: 1, energy: 15},
-          effect: {intelligence: 10, happiness: 1, relationships: 2}
+          effect: {
+            approved: { intelligence: 10, happiness: 1, relationships: 2 },
+            rejected: {}
+          }
         }
       ]
     }
@@ -288,6 +314,7 @@ body {
   outline: none;
   border: 10px solid #F7F6F6;
   border-style: double;
+  cursor: pointer;
 }
 
 .action-button#accept-button {
